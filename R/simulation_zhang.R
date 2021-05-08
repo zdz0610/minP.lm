abfbd15e815949de0d32481506d3a8576fb2ecc8
zdz0610 <dazheng.zhang@pennmedicine.upenn.edu>
2021-05-08 10:06:42 -0400

    errror checking
    error checking for the replicate issues.

--- R/simulation_zhang.R
+++ R/simulation_zhang.R
@@ -24,45 +24,68 @@ simulation_zhang<-function(n_sim,parallel_state=FALSE,random_seed=123,beta_0,bet
   if (!((length(random_seed) == 1L) && is.finite(random_seed) ))
     stop("'random_seed' must be a number")
 
-  data_sim<-NULL
-  if(parallel_state){
-    RNGkind("L'Ecuyer-CMRG")
-    set.seed (random_seed) # Set seed
-    if (Sys.info()[1] == "Windows"){
-      cl = parallel::makeCluster(detectCores()-2)
-      parallel::clusterSetRNGStream(cl, iseed=random_seed)
-      data_sim= parallel::parLapply(cl, 1:n_sim,one_time_simulation,beta_0=beta_0,beta_1=beta_1,beta_2=beta_2,mu=mu,sigma=sigma,na=na,nb=nb,permuted=permuted)
-      parallel::stopCluster(cl)
-    } else {
-      data_sim= parallel::mclapply (1:n_sim,one_time_simulation,beta_0=beta_0,beta_1=beta_1,beta_2=beta_2,mu=mu,sigma=sigma,na=na,nb=nb,permuted=permuted,mc.cores=detectCores())
+#error checking for any replicate issues
+  out <- tryCatch(
+    {
+      data_sim<-NULL
+      if(parallel_state){
+        RNGkind("L'Ecuyer-CMRG")
+        set.seed (random_seed) # Set seed
+        if (Sys.info()[1] == "Windows"){
+          cl = parallel::makeCluster(detectCores()-2)
+          parallel::clusterSetRNGStream(cl, iseed=random_seed)
+          data_sim= parallel::parLapply(cl, 1:n_sim,one_time_simulation,beta_0=beta_0,beta_1=beta_1,beta_2=beta_2,mu=mu,sigma=sigma,na=na,nb=nb,permuted=permuted)
+          parallel::stopCluster(cl)
+        } else {
+          data_sim= parallel::mclapply (1:n_sim,one_time_simulation,beta_0=beta_0,beta_1=beta_1,beta_2=beta_2,mu=mu,sigma=sigma,na=na,nb=nb,permuted=permuted,mc.cores=detectCores())
+        }
+
+        data_sim<-data.frame(matrix(unlist(data_sim), nrow=length(data_sim), byrow=TRUE))
+        if(permuted){
+          colnames(data_sim) <- c("permuted_minP","observed_minP","p_1","p_2","chosen","T_1","T_2")
+        }else{
+          colnames(data_sim) <- c("observed_minP","p_1","p_2","chosen","T_1","T_2")
+        }
+
+        data_sim
+
+
+      }else{
+        set.seed(random_seed)
+        for(sim_times in 1:n_sim){
+          temp<-one_time_simulation(beta_0=beta_0,beta_1=beta_1,beta_2=beta_2,mu=mu,sigma=sigma,na=na,nb=nb,permuted=permuted)
+          data_sim<-rbind(data_sim,temp)
+
+        }
+        data_sim<-as.data.frame(data_sim)
+        if(permuted){
+          colnames(data_sim) <- c("permuted_minP","observed_minP","p_1","p_2","chosen","T_1","T_2")
+        }else{
+          colnames(data_sim) <- c("observed_minP","p_1","p_2","chosen","T_1","T_2")
+        }
+
+        data_sim
+      }
+
+    },
+    error=function(cond) {
+      message(paste("URL does not seem to exist:", url))
+      message("Here's the original error message:")
+      message(cond)
+      # Choose a return value in case of error
+      return(NA)
+    },
+    warning=function(cond) {
+      message(paste("URL caused a warning:", url))
+      message("Here's the original warning message:")
+      message(cond)
+      # Choose a return value in case of warning
+      return(NULL)
     }
+  )
 
-    data_sim<-data.frame(matrix(unlist(data_sim), nrow=length(data_sim), byrow=TRUE))
-    if(permuted){
-      colnames(data_sim) <- c("permuted_minP","observed_minP","p_1","p_2","chosen","T_1","T_2")
-    }else{
-      colnames(data_sim) <- c("observed_minP","p_1","p_2","chosen","T_1","T_2")
-    }
+  return(out)
 
-    return(data_sim)
-    #sapply (op, function (x) x$draw)
-    #sapply (op, function (x) x$arg)
 
-  }else{
-    set.seed(random_seed)
-    for(sim_times in 1:n_sim){
-      temp<-one_time_simulation(beta_0=beta_0,beta_1=beta_1,beta_2=beta_2,mu=mu,sigma=sigma,na=na,nb=nb,permuted=permuted)
-      data_sim<-rbind(data_sim,temp)
-
-    }
-    data_sim<-as.data.frame(data_sim)
-    if(permuted){
-      colnames(data_sim) <- c("permuted_minP","observed_minP","p_1","p_2","chosen","T_1","T_2")
-    }else{
-      colnames(data_sim) <- c("observed_minP","p_1","p_2","chosen","T_1","T_2")
-    }
-
-    return(data_sim)
-  }
 
 }
